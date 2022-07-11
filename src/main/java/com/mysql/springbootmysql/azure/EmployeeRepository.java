e3f3bde0a3076fba479069e85d17409009b95b5a
sonal <sonaliinindiaa@gmail.com>
2022-07-11 11:42:11 +0530

    first commit

---
+++ src/main/java/com/mysql/springbootmysql/azure/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.mysql.springbootmysql.azure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+}
